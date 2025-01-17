6391190ef49312ba8a23793ff2da6ba0ff9b7d73
YIFAN LIU <evannliu@amazon.com>
2025-01-17 11:31:26 -0800

    add opening Q panel

--- plugins/amazonq/codewhisperer/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codewhisperer/speechtotext/SpeechToTextProcessor.kt
+++ plugins/amazonq/codewhisperer/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codewhisperer/speechtotext/SpeechToTextProcessor.kt
@@ -5,13 +5,17 @@ package software.aws.toolkits.jetbrains.services.codewhisperer.speechtotext
 
 import com.google.cloud.speech.v1.*;
 import com.google.protobuf.ByteString;
+import com.intellij.ide.impl.ProjectUtil.getActiveProject
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowManager
+import software.aws.toolkits.jetbrains.services.amazonq.toolwindow.AMAZON_Q_WINDOW_ID
 import software.aws.toolkits.jetbrains.services.codewhisperer.service.CodeWhispererService
 
 
 object SpeechToTextProcessor {
     @Throws(Exception::class)
     fun processAudio(audioData: ByteArray?) {
-        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "/Users/yuxqiang/real-codewhisperer-a466656d2f33.json");
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "/Users/evannliu/Documents/real-codewhisperer-c32330b73659.json");
         SpeechClient.create().use { speechClient ->
             val audioBytes: ByteString = ByteString.copyFrom(audioData)
             val config: RecognitionConfig = RecognitionConfig.newBuilder()
@@ -30,8 +34,44 @@ object SpeechToTextProcessor {
                 val prompt = result.getAlternatives(0).getTranscript()
                 println("Transcription: " + prompt)
                 CodeWhispererService.getInstance().prompt = prompt
-                CodeWhispererService.getInstance().in
+                handleTranscription(prompt)
             }
         }
     }
+    private fun handleTranscription(prompt: String) {
+        val lowerPrompt = prompt.lowercase()
+
+        when {
+            "hi q" in lowerPrompt -> {
+                openQChatPanel()
+            }
+            "chat" in lowerPrompt -> {
+                // callChatApi()
+            }
+            "inline" in lowerPrompt -> {
+                // callInlineCompletionApi()
+            }
+            "generate test" in lowerPrompt -> {
+                // callUnitTestGeneration()
+            }
+            "generate docs" in lowerPrompt -> {
+                //callGenerateDocs()
+            }
+            else -> {
+                println("No matching case for transcription: $prompt")
+            }
+        }
+    }
+
+    private fun openQChatPanel() {
+        println("try to Amazon Q Chat Panel ...")
+        val project: Project? = getActiveProject()
+        if (project != null) {
+            val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(AMAZON_Q_WINDOW_ID)
+            toolWindow?.activate(null , true )
+            println("Amazon Q Chat Panel is opened ...")
+
+        }
+    }
+
 }
